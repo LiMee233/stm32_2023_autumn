20ee96d30d8a45e255666b323674cf79f03b3bf6
LiMee233 <LiMee233@outlook.com>
2023-10-13 11:09:32 +0800

    触摸开关，控制灯的闪烁 * 抄的

--- USER/main.c
+++ USER/main.c
@@ -4,23 +4,29 @@ void delay_ms(__IO uint32_t ms);
 int main(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG,ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE,ENABLE);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOG,&GPIO_InitStructure);
 	GPIO_Init(GPIOB,&GPIO_InitStructure);
 
-	while(1){
-		GPIO_SetBits(GPIOB,GPIO_Pin_8);
-		GPIO_SetBits(GPIOG,GPIO_Pin_7);
-		delay_ms(500);
-		GPIO_ResetBits(GPIOB,GPIO_Pin_8);
-		GPIO_ResetBits(GPIOG,GPIO_Pin_7);
-	  	delay_ms(500);
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	GPIO_SetBits(GPIOB, GPIO_Pin_8 | GPIO_Pin_9);
+
+	while(1)
+	{
+		if(GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_1) == 1)
+		{
+			GPIO_ResetBits(GPIOB, GPIO_Pin_8 | GPIO_Pin_9);
+			delay_ms(500);
+			GPIO_SetBits(GPIOB, GPIO_Pin_8 | GPIO_Pin_9);
+			delay_ms(500);
+		}
 	}
 }
 
