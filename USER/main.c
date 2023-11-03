6460962ea578b6510460910ceb5cf41f19397dcc
LiMee233 <LiMee233@outlook.com>
2023-11-03 08:31:00 +0800

    删除重复的模板内容，添加 GPIO_Mode_IPU

--- USER/main.c
+++ USER/main.c
@@ -4,22 +4,18 @@
 int main(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOG,ENABLE);
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; // Or GPIO_Mode_IPU?
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOG,&GPIO_InitStructure);
+
 	GPIO_Init(GPIOB,&GPIO_InitStructure);
 
 	while(1){
 		GPIO_SetBits(GPIOB,GPIO_Pin_8);
-		GPIO_SetBits(GPIOG,GPIO_Pin_7);
 		delay_ms(500);
 		GPIO_ResetBits(GPIOB,GPIO_Pin_8);
-		GPIO_ResetBits(GPIOG,GPIO_Pin_7);
 	  	delay_ms(500);
 	}
 }
