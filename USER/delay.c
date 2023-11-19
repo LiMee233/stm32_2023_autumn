592a793501fa05e6b6484ee8d4cf6646396e4319
LiMee233 <LiMee233@outlook.com>
2023-11-19 11:38:59 +0800

    修正格式，删除无用代码

--- USER/delay.c
+++ USER/delay.c
@@ -3,11 +3,11 @@
 
 void delay_ms(__IO uint32_t ms)
 {
-    uint32_t i;
-    SysTick_Config(SystemCoreClock / 1000);
-    for(i = 0; i < ms; i++)
-    {
-        while( !((SysTick->CTRL) & SysTick_CTRL_COUNTFLAG_Msk) );
-    }
-    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+	uint32_t i;
+	SysTick_Config(SystemCoreClock / 1000);
+	for(i=0; i<ms; i++)
+	{
+		while(!((SysTick->CTRL) & SysTick_CTRL_COUNTFLAG_Msk));
+	}
+	SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
 }
--- USER/oledfont.h
+++ USER/oledfont.h
@@ -1,6 +1,4 @@
 #include "stm32f10x.h"
 
-extern  unsigned char codeBMP2[];
-extern  unsigned char codeF8X16[];
-extern  unsigned char codeF16x16[];
-extern  unsigned char codeBMP1[];
+extern unsigned char codeF8X16[];
+extern unsigned char codeF16x16[];
